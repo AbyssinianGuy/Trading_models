9175392f8bc354f24a7d4ce2f77c37014ce41807
Michael Asfaw <72876485+AbyssinianGuy@users.noreply.github.com>
2023-04-23 18:51:05 -0400

    commiting only codes
    Signed-off-by: Michael Asfaw <72876485+AbyssinianGuy@users.noreply.github.com>

--- Data/preprocessing.py
+++ Data/preprocessing.py
@@ -1,52 +1,48 @@
-# this is where the preprocessing happens
-
-import pandas as pd
 import numpy as np
 import json
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 
+# Load data
 with open('train_data.json', 'r') as f:
     data = json.load(f)
 
-# convert the data to a panads dataframe
-df = pd.DataFrame(data)
+# Preprocess data
+values_list = [value for values in data.values() for value in values]
+data_points = np.array([[float(val['open_price']), float(val['close_price']), float(val['high_price']),
+                         float(val['low_price']), float(val['volume'])] for val in values_list])
 
-# convert the timestamp column to a datetime and set it as the index
-df['begins_at'] = pd.to_datetime(df['begins_at'], unit='s')
-df.set_index('begins_at', inplace=True)
+# Scale the features
+scaler = MinMaxScaler()
+data_points = scaler.fit_transform(data_points)
 
-# Remove unnecessary columns
-df.drop(['interpolated', 'session', 'volume'], axis=1, inplace=True)
+# Create sequences using sliding window approach
+sequence_length = 251
+step_size = 5  # How many steps to take between each sequence (1 = no overlap)
+sequences = []
+for i in range(0, len(data_points) - sequence_length, step_size):
+    sequences.append(data_points[i:i + sequence_length])
 
-# Handle missing data
-df.fillna(method='ffill', inplace=True)
+sequences = np.array(sequences)
 
-# Normalize the data
-min_vals = df.min()
-max_vals = df.max()
-df = (df - min_vals) / (max_vals - min_vals)
+# Split data into training and validation sets
+train_size = int(0.8 * len(sequences))
+X_train, X_val = sequences[:train_size, :-1], sequences[train_size:, :-1]
+y_train, y_val = sequences[:train_size, -1, 1], sequences[train_size:, -1, 1]  # Assuming target is close price
 
-# create input and target sequences
-input_seq = []
-target_seq = []
-seq_len = 60
+# Reshape target arrays
+y_train = y_train.reshape(-1, 1)
+y_val = y_val.reshape(-1, 1)
 
-for i in range(len(df) - seq_len):
-    input_seq.append(df[i:i+seq_len])
-    target_seq.append(df['close_price'][i+seq_len])
+print("X_train shape:", X_train.shape)
+print("y_train shape:", y_train.shape)
+print("X_val shape:", X_val.shape)
+print("y_val shape:", y_val.shape)
 
-# convert the sequences to numpy arrays
-x = np.array(input_seq)
-y = np.array(target_seq)
+# Save the data
+np.save('Processed/X_train.npy', X_train)
+np.save('Processed/y_train.npy', y_train)
+np.save('Processed/X_val.npy', X_val)
+np.save('Processed/y_val.npy', y_val)
 
-# split the data into training and validation sets
-train_size = int(len(x) * 0.8)
-x_train = x[:train_size]
-y_train = y[:train_size]
-x_valid = x[train_size:]
-y_valid = y[train_size:]
-
-# save the data
-np.save('Data/Processed/x_train.npy', x_train)
-np.save('Data/Processed/y_train.npy', y_train)
-np.save('Data/Processed/x_valid.npy', x_valid)
-np.save('Data/Processed/y_valid.npy', y_valid)
+print("Data saved to disk")
