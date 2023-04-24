cd889f3004280c65f6fcb1cfc501caeb060bf78a
Michael Asfaw <72876485+AbyssinianGuy@users.noreply.github.com>
2023-04-24 19:50:06 -0400

    fixed some bugs on the processing file
    Signed-off-by: Michael Asfaw <72876485+AbyssinianGuy@users.noreply.github.com>

--- AI/train_ai.py
+++ AI/train_ai.py
@@ -5,48 +5,48 @@ from keras.layers import Bidirectional, Dropout, LSTM, Dense
 from tensorflow.python.keras.models import Sequential, load_model
 
 # Load the data
-# X_train = np.load('../Data/Processed/X_train.npy')
-# y_train = np.load('../Data/Processed/y_train.npy')
+X_train = np.load('../Data/Processed/X_train.npy')
+y_train = np.load('../Data/Processed/y_train.npy')
 X_val = np.load('../Data/Processed/X_val.npy')
-# y_val = np.load('../Data/Processed/y_val.npy')
+y_val = np.load('../Data/Processed/y_val.npy')
 
 # Modify the target array shapes
-# y_train = y_train[:, -1, :]
-# y_val = y_val[:, -1, :]
+# y_train = y_train
+# y_val = y_val
 
 # Define the model
-# model = Sequential([
-#     Bidirectional(LSTM(128, input_shape=(250, 5), return_sequences=True)),
-#     Dropout(0.2),
-#     LSTM(64, return_sequences=True),
-#     Dropout(0.2),
-#     LSTM(32),
-#     Dropout(0.2),
-#     Dense(1)
-# ])
+model = Sequential([
+    Bidirectional(LSTM(128, input_shape=(250, 5), return_sequences=True)),
+    Dropout(0.2),
+    LSTM(64, return_sequences=True),
+    Dropout(0.2),
+    LSTM(32),
+    Dropout(0.2),
+    Dense(1)
+])
 
 # Compile the model
-# model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])  # mean_absolute_error
+model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])  # mean_absolute_error
 
 # Train the model
-# model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_val, y_val))
+model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_val, y_val))
 
 # Evaluate the model
-# eval_results = model.evaluate(X_val, y_val)
+eval_results = model.evaluate(X_val, y_val)
 
-# mse, mae = eval_results
+mse, mae = eval_results
 
-# print("Mean squared error:", mse)
-# print("Mean absolute error:", mae)
+print("Mean squared error:", mse)
+print("Mean absolute error:", mae)
 
 # Save the model
-# model.save('model.h5')
+model.save('model-lstm')
 #
 # print("Model saved to disk")
 
 # Load the model
-model = load_model('model.h5')
+# model = load_model('model.h5')
 
-# Make predictions
-predictions = model.predict(X_val)
-print(predictions)
+# Make prediction
+# predictions = model.predict(X_val)
+# print(predictions)
--- Data/preprocessing.py
+++ Data/preprocessing.py
@@ -1,48 +1,70 @@
+import pandas as pd
 import numpy as np
 import json
+from datetime import datetime
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
 
-# Load data
 with open('train_data.json', 'r') as f:
     data = json.load(f)
 
-# Preprocess data
-values_list = [value for values in data.values() for value in values]
-data_points = np.array([[float(val['open_price']), float(val['close_price']), float(val['high_price']),
-                         float(val['low_price']), float(val['volume'])] for val in values_list])
+symbols = list(data.keys())
+values_list = [value for value in data.values()]
 
-# Scale the features
-scaler = MinMaxScaler()
-data_points = scaler.fit_transform(data_points)
+# Extract the relevant information from each entry in the JSON data
+features_list = []
+target_list = []
+date_format = '%Y-%m-%dT%H:%M:%SZ'
 
-# Create sequences using sliding window approach
-sequence_length = 251
-step_size = 5  # How many steps to take between each sequence (1 = no overlap)
-sequences = []
-for i in range(0, len(data_points) - sequence_length, step_size):
-    sequences.append(data_points[i:i + sequence_length])
+# print((values_list[0][0]['begins_at']))
 
-sequences = np.array(sequences)
+for values in values_list:
+    for val in values:
+        if val is not None:
+            dt = datetime.strptime(val['begins_at'], date_format)
+            timestamp_seconds = dt.timestamp()
+            features_list.append([
+                timestamp_seconds,
+                float(val['open_price']),
+                float(val['high_price']),
+                float(val['low_price']),
+                val['volume']
+            ])
+            target_list.append(float(val['close_price']))
+# print(features_list[0])
+features_array = np.array(features_list)
+target_array = np.array(target_list)
 
-# Split data into training and validation sets
-train_size = int(0.8 * len(sequences))
-X_train, X_val = sequences[:train_size, :-1], sequences[train_size:, :-1]
-y_train, y_val = sequences[:train_size, -1, 1], sequences[train_size:, -1, 1]  # Assuming target is close price
+# Ensure that the total number of rows in features_array is divisible by the data_point_per_ticker
+total_rows = features_array.shape[0]
+data_point_per_ticker = 251
+extra_rows = total_rows % data_point_per_ticker
 
-# Reshape target arrays
-y_train = y_train.reshape(-1, 1)
-y_val = y_val.reshape(-1, 1)
+if extra_rows != 0:
+    features_array = features_array[:-extra_rows]
+    target_array = target_array[:-extra_rows]
 
-print("X_train shape:", X_train.shape)
-print("y_train shape:", y_train.shape)
-print("X_val shape:", X_val.shape)
-print("y_val shape:", y_val.shape)
+print("Features array shape:", features_array.shape)
+print("Target array shape:", target_array.shape)
 
-# Save the data
-np.save('Processed/X_train.npy', X_train)
-np.save('Processed/y_train.npy', y_train)
-np.save('Processed/X_val.npy', X_val)
-np.save('Processed/y_val.npy', y_val)
+# Reshape the 2D arrays into 3D arrays with shape (10703, 251, 5) and (10703, 251, 1)
+X = features_array.reshape((-1, data_point_per_ticker, 5))
+y = target_array.reshape((-1, data_point_per_ticker, 1))
 
-print("Data saved to disk")
+print("Shape of X:", X.shape)
+print("Shape of y:", y.shape)
+
+# Define the size of the validation set
+validation_size = 0.2
+
+# Split the data into training and validation sets
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=validation_size, random_state=42)
+
+print("Shape of X_train:", X_train.shape)
+print("Shape of X_val:", X_val.shape)
+print("Shape of y_train:", y_train.shape)
+print("Shape of y_val:", y_val.shape)
+
+np.save('X_train.npy', X_train)
+np.save('X_val.npy', X_val)
+np.save('y_train.npy', y_train)
+np.save('y_val.npy', y_val)
